806dda4ca0e1381020e51ef1dffb22b892c82931
ARCHIVE-code21 <solarpark7346@gmail.com>
2021-01-12 16:32:31 +0900

    test

---
+++ BS/archive_sns/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer_content() {
+    return(
+        
+    <div className="footer">
+    
+    Copyright © 2020 ARCHIVE ALL Rights Reserved.
+
+    </div>
+    );
+    }
+
+    export default Footer_content;
---
+++ BS/archive_sns/src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Login } from './LoginForm';
+export { default as home } from './home';
